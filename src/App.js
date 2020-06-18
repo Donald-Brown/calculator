10bd087a6058b42ff1d1c0f752c10c0051d43b76
Donald Brown <donbrown4652@gmail.com>
2020-06-18 16:09:55 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div className="calculator">
+    <div id="display" className="display">
+      3.14159
     </div>
-  );
-}
+    <button className="light-grey">1</button>
+    <button className="light-grey">2</button>
+    <button className="light-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+    <button className="dark-grey">1</button>
+    <button className="dark-grey">2</button>
+    <button className="dark-grey">3</button>
+    <button className="orange">4</button>
+  </div>
+);
 
 export default App;
